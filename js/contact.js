dcbf72607697adc0310e13ef62e6e1715fc38541
Vilenan <vilenan@mail.ru>
2020-11-09 11:39:04 +0300

    Защита. Исправления.
    за

--- js/contact.js
+++ js/contact.js
@@ -37,3 +37,25 @@ window.addEventListener("keydown", function(evt){
        }
    }
 });
+
+const cartLink = document.querySelector(".product-image .button-buy");
+const cartPopup = document.querySelector(".modal-cart");
+const cartClose = cartPopup.querySelector(".modal-close");
+
+cartLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.add("modal-show");
+});
+cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function(evt){
+   if (evt.keyCode === 27){
+       if(cartPopup.classList.contains("modal-show")){
+          evt.preventDefault();
+          cartPopup.classList.remove("modal-show");
+       }
+   }
+});
