88fd4e1b252dc5c2d7f9eb87640666843700ca77
Vilenan <vilenan@mail.ru>
2020-11-09 15:36:45 +0300

    изображения

--- js/contact.js
+++ js/contact.js
@@ -40,17 +40,18 @@ window.addEventListener("keydown", function(evt){
 
 const cartLink = document.querySelector(".product-image .button-buy");
 const cartPopup = document.querySelector(".modal-cart");
+if (cartPopup) {
 const cartClose = cartPopup.querySelector(".modal-close");
+cartClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    cartPopup.classList.remove("modal-show");
+});
+}
 
 cartLink.addEventListener("click", function (evt) {
     evt.preventDefault();
     cartPopup.classList.add("modal-show");
 });
-cartClose.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    cartPopup.classList.remove("modal-show");
-});
-
 window.addEventListener("keydown", function(evt){
    if (evt.keyCode === 27){
        if(cartPopup.classList.contains("modal-show")){
